7adafaa492bf29fbcf04dbe68443b1599b7f069b
limeizheng <limeizhengchn@gmail.com>
2023-05-29 15:12:26 -0400

    finish lab04

--- lab04/flik/HorribleSteve.java
+++ lab04/flik/HorribleSteve.java
@@ -1,8 +1,10 @@
 package flik;
 
+
 public class HorribleSteve {
     public static void main(String [] args) throws Exception {
         int i = 0;
+
         for (int j = 0; i < 500; ++i, ++j) {
             if (!Flik.isSameNumber(i, j)) {
                 throw new Exception(
